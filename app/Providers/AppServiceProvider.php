f5d87b4d7755c032faac60bb78e03932eab22b51
RiyanStudioFlow <r741622@gmail.com>
2026-06-20 20:25:43 +0700

    fix: force https scheme for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Iki tambahan penting 1
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Iki tambahan penting 2: Mekso rute dadi HTTPS pas online
+        if (config('app.env') === 'production' || env('PORT')) {
+            URL::forceScheme('https');
+        }
     }
 }
