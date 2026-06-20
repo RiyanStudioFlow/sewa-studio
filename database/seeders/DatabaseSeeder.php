0dc0de71173ad985b9793dce745d3d9c85f79c8e
RiyanStudioFlow <r741622@gmail.com>
2026-06-21 04:40:29 +0700

    fix: kosongkan seeder karena data admin sudah ada di database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,35 +3,13 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Pake updateOrCreate ben lek akun wis ono nang MySQL, gak nabrak/error maneh
-        
-        // 1. AKUN ADMIN
-        User::updateOrCreate(
-            ['email' => 'admin123@gmail.com'], // Patokane email
-            [
-                'name' => 'Admin LensFlow Studio',
-                'password' => Hash::make('rahasia456'),
-                'role' => 'admin',
-                'email_verified_at' => now(),
-            ]
-        );
-
-        // 2. AKUN PELANGGAN
-        User::updateOrCreate(
-            ['email' => 'pelanggan@gmail.com'], // Patokane email
-            [
-                'name' => 'Riyan Pelanggan',
-                'password' => Hash::make('pelanggan123'),
-                'role' => 'pelanggan',
-                'email_verified_at' => now(),
-            ]
-        );
+        // KOSONGIN WAE BRO! 
+        // Akun admin wis sukses mlebu nang database MySQL sakdurunge.
+        // Iki dikosongno ben Railway gak nyoba nyuntik data dobel pas deploy!
     }
 }
